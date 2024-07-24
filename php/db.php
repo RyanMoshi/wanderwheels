9734a9d0f499bd983bd68489cf3604ea92747728
RyanMoshi <ryan.moshi@strathmore.edu>
2024-07-24 11:34:01 +0300

    db.php changes

--- php/db.php
+++ php/db.php
@@ -1,22 +1,4 @@
 <?php
-<<<<<<< HEAD
-// Get the form data
-
-
-// Connect to our database (our database credentials)
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "wanderwheels";
-$port = "3306";
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die('Connection Failed : '. $conn->connect_error);
-} else {
-    // Connection works
-
-=======
 $host = 'localhost';
 $db = 'wanderwheels';
 $user = 'root';
@@ -35,6 +17,5 @@ try {
     $pdo = new PDO($dsn, $user, $password, $options);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
->>>>>>> 0a0202ec7c37893e352786ef9a25d3cf2b09d8ac
 }
 ?>
